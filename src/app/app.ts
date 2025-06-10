904d711cf73d667b60eae2d02f8120303b315a1f
Kim <kimbfsd@gmail.com>
2025-06-10 16:19:34 +0300

    workinng init

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
 import { RouterOutlet } from '@angular/router';
+
+declare const google: any;
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, RouterOutlet], 
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected title = 'temp-angular-project';
+export class AppComponent implements AfterViewInit {
+  title = 'MapPal';
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+
+  initMap(): void {
+
+    const haifaCoordinates = { lat: 32.7940, lng: 34.9896 };
+
+    const mapOptions = {
+      center: haifaCoordinates,
+      zoom: 12
+    };
+
+    const mapElement = document.getElementById('map');
+    if (mapElement) {
+      const map = new google.maps.Map(mapElement, mapOptions);
+      console.log('Google Map successfully initialized!');
+    } else {
+      console.error('Error: Map element not found.');
+    }
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import { AppComponent } from './app/app'; // <--- ИЗМЕНИТЬ ЗДЕСЬ App на AppComponent
 
-bootstrapApplication(App, appConfig)
+bootstrapApplication(AppComponent, appConfig) // <--- ИЗМЕНИТЬ ЗДЕСЬ App на AppComponent
   .catch((err) => console.error(err));
